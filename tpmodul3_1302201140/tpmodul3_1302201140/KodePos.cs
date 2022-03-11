cb7d3ebfc2d0c8b92c13a175c512ae4366d05322
agistaputri20 <agistaputri@student.telkomuniversity.ac.id>
2022-03-11 11:08:50 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302201140/tpmodul3_1302201140/KodePos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302201140
+{
+    internal class KodePos
+    {
+        public static void getKodePos(string kel, string kode)
+        {
+            Console.WriteLine(kel + " " + kode);
+        }
+
+    }
+}
